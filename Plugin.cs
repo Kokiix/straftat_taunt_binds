d9b07f0372dc1a16920e3918f939b83ecb2a11b1
kenneth <kokiidoki@gmail.com>
2026-05-19 09:46:03 -0400

    touch up main plugin file

--- Plugin.cs
+++ Plugin.cs
@@ -10,15 +10,15 @@ using UnityEngine;
 
 namespace TauntBinds;
 
-[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInPlugin("com.koki.tauntbinds", "Taunt Binds", "1.0.0")]
 public class Plugin : BaseUnityPlugin
 {
     internal static ManualLogSource Log;
     internal static Harmony harmony;
 
-    private static ConfigEntry<KeyCode>[] tauntKeys = new ConfigEntry<KeyCode>[10];
-    private static readonly KeyCode[] defaultKeys = [KeyCode.Alpha0, KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9];
-    private static readonly float[] tauntDurations = [0.4f, 0.3f, 0.3f, 0.5f, 0.7f, 0.4f, 0.7f, 0.9f, 1f, 0.3f];
+    private static ConfigEntry<KeyCode>[] _tauntKeys = new ConfigEntry<KeyCode>[10];
+    private static readonly KeyCode[] _defaultKeys = [KeyCode.Alpha0, KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9];
+    private static readonly float[] _tauntDurations = [0.4f, 0.3f, 0.3f, 0.5f, 0.7f, 0.4f, 0.7f, 0.9f, 1f, 0.3f];
 
     private void Awake()
     {
@@ -28,27 +28,25 @@ public class Plugin : BaseUnityPlugin
 
         for (int i = 0; i < 10; i++)
         {
-            tauntKeys[i] = Config.Bind("General", "Key for taunt #" + i, defaultKeys[i]);
+            _tauntKeys[i] = Config.Bind("General", "Key for taunt #" + i, _defaultKeys[i]);
         }
     }
 
-    [HarmonyPatch(typeof(FirstPersonController))]
+    [HarmonyPatch(typeof(FirstPersonController), "HandleTaunt")]
     internal static class FirstPersonControllerPatch
     {
-        [HarmonyPatch("HandleTaunt")]
-        [HarmonyPrefix]
-        public static bool HandleTaunt(FirstPersonController __instance)
+        public static bool Prefix(FirstPersonController __instance)
         {
             __instance.tauntTimer -= Time.deltaTime;
             if (__instance.tauntTimer > 0f) return false;
 
             for (int i = 0; i < 10; i++)
             {
-                if (Input.GetKeyDown(tauntKeys[i].Value))
+                if (Input.GetKeyDown(_tauntKeys[i].Value))
                 {
                     __instance.AboubiPlayServer(i);
                     Settings.Instance.IncreaseTauntsAmount();
-                    __instance.tauntTimer = tauntDurations[i];
+                    __instance.tauntTimer = _tauntDurations[i];
                     break;
                 }
             }
