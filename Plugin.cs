600cc82c07184adf4de18723b1621c7bd2e93655
kenneth <kokiidoki@gmail.com>
2026-05-19 10:30:13 -0400

    realize that there's an if statement jumping over my function
    ALSO: my taunt binds have been off by 1

--- Plugin.cs
+++ Plugin.cs
@@ -17,7 +17,7 @@ namespace TauntBinds;
 public class TauntBindPlugin : BaseUnityPlugin
 {
     internal static ManualLogSource Log;
-    internal static Harmony harmony;
+    private static Harmony _harmony;
 
     private static ConfigEntry<KeyCode>[] _tauntKeys = new ConfigEntry<KeyCode>[10];
     private static readonly KeyCode[] _defaultKeys = [KeyCode.Alpha0, KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9];
@@ -27,9 +27,6 @@ public class TauntBindPlugin : BaseUnityPlugin
     private void Awake()
     {
         Log = Logger;
-        harmony = new Harmony("TauntBinds");
-        harmony.PatchAll();
-
         for (int i = 0; i < 10; i++)
         {
             _tauntKeys[i] = Config.Bind("General", "Key for taunt #" + i, _defaultKeys[i]);
@@ -43,42 +40,47 @@ public class TauntBindPlugin : BaseUnityPlugin
             // Other mod has a prefix that just totally rewrites the function so it can't be compatible
             Harmony.UnpatchID("dimolade.harmony.InfTaunt");
         }
-    }
-
-    [HarmonyPatch(typeof(FirstPersonController), "HandleTaunt")]
-    public static class FirstPersonControllerPatch
-    {
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            var handleTaunt = AccessTools.Method(typeof(FirstPersonControllerPatch), nameof(HandleTaunt));
-
-            return new CodeMatcher(instructions)
-            .MatchForward(useEnd: false, new CodeMatch(OpCodes.Ldc_I4_S, 49))
-            .Insert(
-                new CodeInstruction(OpCodes.Ldarg_0),
-                new CodeInstruction(OpCodes.Call, handleTaunt),
-                new CodeInstruction(OpCodes.Ret))
-            .InstructionEnumeration();
-        }
-
-        public static void HandleTaunt(FirstPersonController __instance)
-        {
-            Debug.LogError("SLDFKJ");
-            for (int i = 0; i < 10; i++)
-            {
-                if (Input.GetKeyDown(_tauntKeys[i].Value))
-                {
-                    __instance.AboubiPlayServer(i);
-                    Settings.Instance.IncreaseTauntsAmount();
-                    __instance.tauntTimer = _infTaunt ? 0 : _tauntCooldowns[i];
-                    break;
-                }
-            }
-        }
+        
+        _harmony = new Harmony("TauntBinds");
+        _harmony.PatchAll();
     }
 
     private void OnDestroy()
     {
-        harmony.UnpatchSelf();
+        Debug.LogError("destroying");
+        _harmony.UnpatchSelf();
+    }
+}
+
+[HarmonyPatch(typeof(FirstPersonController), "HandleTaunt")]
+public static class FirstPersonControllerPatch
+{
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        Debug.LogError("SLDFKJ");
+        var handleTaunt = AccessTools.Method(typeof(FirstPersonControllerPatch), nameof(HandleTaunt));
+
+        return new CodeMatcher(instructions)
+        .MatchForward(useEnd: false, new CodeMatch(OpCodes.Ldc_I4_S, 49))
+        .Insert(
+            new CodeInstruction(OpCodes.Ldarg_0),
+            new CodeInstruction(OpCodes.Call, handleTaunt),
+            new CodeInstruction(OpCodes.Ret))
+        .InstructionEnumeration();
+    }
+
+    public static void HandleTaunt(FirstPersonController __instance)
+    {
+        TauntBindPlugin.Log.LogError("SLFKDJ");
+        // for (int i = 0; i < 10; i++) 
+        // {
+        //     if (Input.GetKeyDown(_tauntKeys[i].Value))
+        //     {
+        //         __instance.AboubiPlayServer(i);
+        //         Settings.Instance.IncreaseTauntsAmount();
+        //         __instance.tauntTimer = _infTaunt ? 0 : _tauntCooldowns[i];
+        //         break;
+        //     }
+        // }
     }
 }
